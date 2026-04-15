b858823dfeda51681ba2bdbf07d9829f9bfc16a7
Ethan McEvoy <123204154+EMcE01@users.noreply.github.com>
2026-04-14 21:57:29 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+# FAQ of a smart watch
+A simple static website that swaps between images and expands text boxes when the user clicks on a frequently asked question.
+
+---
+## Author
+- Etmcev01 - Ethan McEvoy
+- brherm05 - Brayden Hermanson
+
+## 🧠 Features
+
+- when clicked ->
+- swaps between 3 images
+- expands and collapses text
+---
+
+## 🛠️ Technologies Used
+
+- HTML
+- CSS
+- JavaScript (Vanilla JS)
+- DOM Manipulation
+- Event Listeners
+
+---
+
+## ScreenShots
+![All Closed](images/allClosed.png)
+![WaterProof](images/waterproof.png)
+![heart rate](images/heartRate.png)
+
+## 🚀 New Skills Learned
+- querySelectorAll()
+- manipulate images using JS
+- manipulate the DOM by Attributes
+
+---
+
---
+++ faqs.js
@@ -0,0 +1,68 @@
+/**
+ * Authors: Ethan McEvoy & Brayden Hermanson
+ * Completion Date: 3/5/2026
+ * Program: Smartwatch FAQ
+ * Summary: This program provides an easy to use UI to read a couple of
+ * potential questions the user may have about the product.
+ */
+
+// SETUP GLOBAL VARIABLES
+const faqImage = document.querySelector("#faqs img");
+const faqImageOrigSrc = faqImage.src;
+const faqImageOrigAlt = faqImage.alt;
+const h2s = document.querySelectorAll("#faqs h2");
+
+// SETUP THE EVENT HANDLER
+const toggleVisibility = evt => {
+    // 1. Which H2 was clicked
+    const ct = evt.currentTarget;
+
+    // 2. Track if everything ends up closed
+    let allClosed = false;
+
+    // 3. Loop through every H2
+    for (const h2 of h2s) {
+
+        if (h2 === ct) {
+            // If already open, track that everything will be closed
+            if (h2.classList.contains("minus")) {
+                allClosed = true;
+            }
+
+            // Toggle classes
+            h2.classList.toggle("minus");
+            h2.nextElementSibling.classList.toggle("open");
+
+            // Get image data from attributes
+            const newSrc = h2.dataset.img;
+            const newAlt = h2.dataset.alt;
+
+            // Update main image
+            faqImage.src = newSrc;
+            faqImage.alt = newAlt;
+
+        } else {
+            // Close other FAQs
+            h2.classList.remove("minus");
+            h2.nextElementSibling.classList.remove("open");
+        }
+
+    };
+
+    // 4. Reset to default image if everything closed
+    if (allClosed) {
+        faqImage.src = faqImageOrigSrc;
+        faqImage.alt = faqImageOrigAlt;
+    }
+
+    // 5. Prevent default browser behavior
+    evt.preventDefault();
+};
+
+// INITIALIZATION
+document.addEventListener("DOMContentLoaded", () => {
+    // Loop through all H2 headings
+    for (const h2 of h2s) {
+        h2.addEventListener("click", toggleVisibility);
+    }
+});
